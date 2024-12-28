fe0a744e2b84cf7184f6b046728f6ddd7a749644
dactrtr <dactrtr@gmail.com>
2024-12-28 21:50:58 +0900

    fixed double rectangle in triggers

--- LevelGenerator/Views/MapView.swift
+++ LevelGenerator/Views/MapView.swift
@@ -24,7 +24,8 @@ struct MapView: View {
                 .stroke(Color.gray, lineWidth: 2)
                 .frame(width: 400, height: 240)
             
-            ForEach(Array(placedItems.enumerated()), id: \.element.id) { index, item in
+            ForEach(Array(placedItems.filter { $0.itemType != .trigger }.enumerated()), 
+                   id: \.element.id) { index, item in
                 ZStack {
                     Image(item.type)
                         .resizable()
@@ -43,16 +44,18 @@ struct MapView: View {
                 .position(x: item.x, y: item.y)
             }
             
-            Image(selectedItem)
-                .resizable()
-                .frame(width: 32, height: 32)
-                .position(x: currentX, y: currentY)
-            
-            Image(selectedEnemy)
-                .resizable()
-                .frame(width: selectedEnemy == "frogcolli" ? 40 : 32,
-                       height: selectedEnemy == "frogcolli" ? 40 : 32)
-                .position(x: enemyX, y: enemyY)
+            if !showTriggerPreview {
+                Image(selectedItem)
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                    .position(x: currentX, y: currentY)
+                
+                Image(selectedEnemy)
+                    .resizable()
+                    .frame(width: selectedEnemy == "frogcolli" ? 40 : 32,
+                           height: selectedEnemy == "frogcolli" ? 40 : 32)
+                    .position(x: enemyX, y: enemyY)
+            }
             
             if showTriggerPreview {
                 Rectangle()
@@ -61,16 +64,27 @@ struct MapView: View {
                     .position(x: triggerX, y: triggerY)
             }
             
-            ForEach(placedItems.filter { $0.itemType == .trigger }) { trigger in
-                Rectangle()
-                    .fill(Color.purple.opacity(0.2))
-                    .frame(width: trigger.width ?? 60, height: trigger.height ?? 30)
-                    .position(x: trigger.x, y: trigger.y)
-                    .overlay(
-                        Rectangle()
-                            .stroke(Color.purple, lineWidth: 1)
-                            .frame(width: trigger.width ?? 60, height: trigger.height ?? 30)
-                    )
+            ForEach(Array(placedItems.filter { $0.itemType == .trigger }.enumerated()), 
+                   id: \.element.id) { index, trigger in
+                ZStack {
+                    Rectangle()
+                        .fill(Color.purple.opacity(0.2))
+                        .frame(width: trigger.width ?? 60, height: trigger.height ?? 30)
+                        .overlay(
+                            Rectangle()
+                                .stroke(Color.purple, lineWidth: 1)
+                        )
+                    
+                    ZStack {
+                        Circle()
+                            .fill(Color.purple)
+                            .frame(width: 16, height: 16)
+                        Text("\(index + 1)")
+                            .foregroundColor(.white)
+                            .font(.system(size: 10, weight: .bold))
+                    }
+                }
+                .position(x: trigger.x, y: trigger.y)
             }
         }
         .frame(width: 400, height: 240)
